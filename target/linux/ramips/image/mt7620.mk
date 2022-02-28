75c4058fdfc8919f96b91e9448f4f35b7dcac26a
gitnewcomer <michaelfsantos@gmail.com>
2022-02-28 23:46:31 +0100

    Add support for Xiaomi MiWifi R3

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -467,8 +467,9 @@ define Device/miwifi-mini
 endef
 TARGET_DEVICES += miwifi-mini
 
-+define Device/miwifi-r3
+define Device/miwifi-r3
   DTS := MIWIFI-R3
+  SOC := mt7620a
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_SIZE := 4096k
